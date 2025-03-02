5122c6b3e6410c4c1edb0a327ee64baca026dbcf
iamtheluiz <iamtheluiz.dev@gmail.com>
2025-03-02 20:16:45 -0300

    feat: add header component and update app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from "./components/container/container.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ContainerComponent],
+  imports: [ContainerComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
