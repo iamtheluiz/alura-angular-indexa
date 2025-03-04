17fc7494baa6b34a1c8ca7057fbe85e81e6cf1fc
iamtheluiz <iamtheluiz.dev@gmail.com>
2025-03-03 23:28:05 -0300

    feat: add contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ContainerComponent } from "./components/container/container.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { SeparatorComponent } from "./components/separator/separator.component";
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ContainerComponent, HeaderComponent, SeparatorComponent],
+  imports: [ContainerComponent, HeaderComponent, SeparatorComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
