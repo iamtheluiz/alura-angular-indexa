36d7c058cf15689923ec0e4d8040df644352fde8
iamtheluiz <iamtheluiz.dev@gmail.com>
2025-03-03 23:04:02 -0300

    feat: add separator component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from "./components/container/container.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { SeparatorComponent } from "./components/separator/separator.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ContainerComponent, HeaderComponent],
+  imports: [ContainerComponent, HeaderComponent, SeparatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'alura-angular-indexa';
+  alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 }
