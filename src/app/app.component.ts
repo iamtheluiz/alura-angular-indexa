a22912c3dc2f736768f9644dc8158ea908749b41
iamtheluiz <iamtheluiz.dev@gmail.com>
2025-03-03 23:40:35 -0300

    feat: add agenda data and implement contact filtering by first letter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,14 @@ import { HeaderComponent } from "./components/header/header.component";
 import { SeparatorComponent } from "./components/separator/separator.component";
 import { ContactComponent } from './components/contact/contact.component';
 
+import agenda from './agenda.json';
+
+interface Contact {
+  id: number;
+  name: string;
+  phone: string;
+}
+
 @Component({
   selector: 'app-root',
   imports: [ContainerComponent, HeaderComponent, SeparatorComponent, ContactComponent],
@@ -12,4 +20,9 @@ import { ContactComponent } from './components/contact/contact.component';
 })
 export class AppComponent {
   alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  contacts: Contact[] = agenda;
+
+  filterContactsByFistLetter(letter: string): Contact[] {
+    return this.contacts.filter(contact => contact.name[0].toUpperCase() === letter.toUpperCase());
+  }
 }
