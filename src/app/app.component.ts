1899b042f3e2c56b299e0139e553e84655c3ed48
iamtheluiz <iamtheluiz.dev@gmail.com>
2025-03-05 15:02:28 -0300

    feat: implement search input for contact filtering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { ContainerComponent } from "./components/container/container.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { SeparatorComponent } from "./components/separator/separator.component";
 import { ContactComponent } from './components/contact/contact.component';
+import { FormsModule } from '@angular/forms';
 
 import agenda from './agenda.json';
 
@@ -14,7 +15,13 @@ interface Contact {
 
 @Component({
   selector: 'app-root',
-  imports: [ContainerComponent, HeaderComponent, SeparatorComponent, ContactComponent],
+  imports: [
+    ContainerComponent,
+    HeaderComponent,
+    SeparatorComponent,
+    ContactComponent,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -22,7 +29,17 @@ export class AppComponent {
   alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   contacts: Contact[] = agenda;
 
+  textFilter = '';
+
+  filterContactsByText(): Contact[] {
+    if (!this.textFilter) {
+      return this.contacts;
+    }
+
+    return this.contacts.filter(contact => contact.name.toLowerCase().includes(this.textFilter.toLowerCase()));
+  }
+
   filterContactsByFistLetter(letter: string): Contact[] {
-    return this.contacts.filter(contact => contact.name[0].toUpperCase() === letter.toUpperCase());
+    return this.filterContactsByText().filter(contact => contact.name[0].toUpperCase() === letter.toUpperCase());
   }
 }
